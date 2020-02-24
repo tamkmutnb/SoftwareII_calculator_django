93a8947bdffbc3625f56b45da6da40b7d89e5631
s6101012620148 <s6101012620148@email.kmutnb.ac.th>
2020-02-24 21:56:41 +0700

    complete template for user input

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
 class HomePageTest(TestCase):
 
--- s6101012620148/urls.py
+++ s6101012620148/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from calculator import views
+
+from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_page),
 ]
+
+'''example
+    path('', views.register, name='register'),
+    path('calGrade', views.calGrade,name='calGrade'),
+    path('termselect', views.termselect,name='termselect'),
+    path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    url(r'home', views.home_page, name='home'),
+    url(r'flow', views.flow, name='flow'),
+    url(r'picFlow', views.picFlow, name='picFlow'),
+    '''

386e1507086b34505a0f6e93c5720a5a9c8d30e3
s6101012620148 <s6101012620148@email.kmutnb.ac.th>
2020-02-24 21:09:24 +0700

    create unit_test and functional_tests files

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
+def home_page(request):
+    return render(request, 'home.html')
